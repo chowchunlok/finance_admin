83ee2ded8459e7f1cf3152e42e7625ca93716dbd
marco <marcozjl3030@gmail.com>
2019-03-20 19:31:44 +0800

    rich-text

--- server/routes/article.js
+++ server/routes/article.js
@@ -5,7 +5,7 @@ var model = require('../db/db')
 //获取文章列表 NOTE: routes > fetch article list
 router.get('/list', function(req, res) {
   model.Article.find({}, (err, doc) => {
-    console.log(doc) //DEBUG:
+    // console.log(doc) //DEBUG:
     if (err) {
       console.log('list_err 8040:', err) //DEBUG:list_err 8040
       res.json({
@@ -15,17 +15,80 @@ router.get('/list', function(req, res) {
     } else if (!doc) {
       res.json({
         code: 8041,
-        message: 'No Data'
+        message: 'Not Found Data'
       })
     } else {
       res.json({
         code: 2000,
         data: {
-          total: 2,
+          total: doc.length,
           items: doc
         }
       })
     }
+  })
+})
+
+// id获取文章详细
+router.get('/details', function(req, res) {
+  let id = req.query.id
+  model.Article.findOne({ id }, (err, doc) => {
+    // console.log(doc) //DEBUG:
+    if (err) {
+      console.log('list_err 8042:', err) //DEBUG:list_err 8040
+      res.json({
+        code: 8042,
+        message: 'Database Error'
+      })
+    } else if (!doc) {
+      res.json({
+        code: 8043,
+        message: 'Not Found Data'
+      })
+    } else {
+      res.json({
+        code: 2000,
+        data: {
+          message: 'Fetch Success',
+          items: doc
+        }
+      })
+    }
+  })
+})
+
+//更新文章
+router.post('/update', function(req, res) {
+  let id = req.body.id
+  model.Article.findOne({ id }, (err, doc) => {
+    if (err) {
+      console.log('update_err 8042:', err) //DEBUG:update_err 8040
+      res.json({
+        code: 8042,
+        message: 'Database Error'
+      })
+    }
+    if (!doc) {
+      res.json({
+        code: 8043,
+        message: 'Not Found Data'
+      })
+    } else {
+      // TODO: routes > update article status
+      res.json({
+        code: 2000,
+        message: 'Update Success',
+        data: doc
+      })
+    }
+  })
+})
+
+//获取作者列表
+router.get('/author/list', function(req, res) {
+  //TODO: mongodb find with key????
+  res.json({
+    code: 2000
   })
 })
 
--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -52,13 +52,6 @@
         icon="el-icon-search"
         @click="handleFilter"
       >{{ $t('table.search') }}</el-button>
-      <el-button
-        class="filter-item"
-        style="margin-left: 10px;"
-        type="primary"
-        icon="el-icon-edit"
-        @click="handleCreate"
-      >{{ $t('table.add') }}</el-button>
       <el-checkbox
         v-model="showReviewer"
         class="filter-item"
@@ -147,11 +140,9 @@
         class-name="small-padding fixed-width"
       >
         <template slot-scope="scope">
-          <el-button
-            type="primary"
-            size="mini"
-            @click="handleUpdate(scope.row)"
-          >{{ $t('table.edit') }}</el-button>
+          <el-button type="primary" size="mini" @click="handleUpdate(scope.row)">
+            <router-link :to="'/article/edit/' + scope.row.id">{{ $t('table.edit') }}</router-link>
+          </el-button>
           <el-button
             v-if="scope.row.status!='published'"
             size="mini"
@@ -261,12 +252,13 @@ import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
 import waves from '@/directive/waves' // Waves directive
 import { parseTime } from '@/utils'
 import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+import { MessageBox } from 'element-ui'
 
 const calendarTypeOptions = [
-  { key: 'CN', display_name: 'China' },
-  { key: 'US', display_name: 'USA' },
-  { key: 'JP', display_name: 'Japan' },
-  { key: 'EU', display_name: 'Eurozone' }
+  { key: 'AD', display_name: 'Advert' },
+  { key: 'DL', display_name: 'Daily' },
+  { key: 'N', display_name: 'News' },
+  { key: 'FN', display_name: 'Finance' }
 ]
 
 // arr to obj ,such as { CN : "China", US : "USA" }
@@ -306,7 +298,7 @@ export default {
         type: undefined,
         sort: '+id'
       },
-      importanceOptions: [1, 2, 3],
+      importanceOptions: ['1', '2', '3'],
       calendarTypeOptions,
       sortOptions: [{ label: 'ID Ascending', key: '+id' }, { label: 'ID Descending', key: '-id' }],
       statusOptions: ['published', 'draft', 'deleted'],
@@ -359,11 +351,33 @@ export default {
       this.getList()
     },
     handleModifyStatus(row, status) {
-      this.$message({
-        message: '操作成功',
-        type: 'success'
-      })
-      row.status = status
+      //NOTE: FIXME: handleModifyStatus --> COMPLETE FEATURE
+      if (status) {
+        this.$confirm(`This Article Will be ${status.toUpperCase()}, Still Go On?`, 'Warning:', {
+          confirmButtonText: 'Yes',
+          cancelButtonText: 'No',
+          type: 'warning',
+          center: true
+        })
+          .then(() => {
+            let id = row.id
+            updateArticle({ id, status })
+              .then(res => {
+                this.$message({
+                  type: 'success',
+                  message: res.message
+                })
+                row.status = status
+              })
+              .catch(err => {
+                this.$message({
+                  type: 'error',
+                  message: err.message
+                })
+              })
+          })
+          .catch(() => {})
+      }
     },
     sortChange(data) {
       const { prop, order } = data
@@ -389,14 +403,6 @@ export default {
         status: 'published',
         type: ''
       }
-    },
-    handleCreate() {
-      this.resetTemp()
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
     },
     createData() {
       this.$refs['dataForm'].validate(valid => {
@@ -464,21 +470,6 @@ export default {
         this.pvData = response.data.pvData
         this.dialogPvVisible = true
       })
-    },
-    handleDownload() {
-      this.downloadLoading = true
-      // import('@/vendor/Export2Excel').then(excel => {
-      //   const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
-      //   const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
-      //   const data = this.formatJson(filterVal, this.list)
-      //   excel.export_json_to_excel({
-      //     header: tHeader,
-      //     data,
-      //     filename: 'table-list'
-      //   })
-      //   this.downloadLoading = false
-      // })
-      console.log('Export2Excel')
     },
     formatJson(filterVal, jsonData) {
       return jsonData.map(v =>
